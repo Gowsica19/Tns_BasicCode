475c2b3d275581039e33d40ccc2fd4711bd4e58e
Gow#1234 <gowsica25@gmail.com>
2025-12-22 21:54:56 +0530

    init commit

---
+++ batch79/src/com/tnsif/wrapperdemo/WrapperClassDemo.java
@@ -0,0 +1,25 @@
+package com.tnsif.wrapperdemo;
+
+public class WrapperClassDemo {
+
+	public static void main(String[] args) {
+	
+		//primitive into object --> autoboxing 
+
+		
+		int x = 10;
+		
+		Integer y = Integer.valueOf(x);
+		
+		
+		System.out.println(y);
+		
+		
+		// object into primitive --> unboxing
+		
+		int z = y;
+		
+		System.out.println(z);
+
+	}
+}
