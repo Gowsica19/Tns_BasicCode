e4e38b45dde45bb8edcb27eb19ae960b2efc893a
Gow#1234 <gowsica25@gmail.com>
2025-12-22 21:43:52 +0530

    init commit

---
+++ batch79/src/com/tnsif/packageandaccessmodifiers/PackageTest.java
@@ -0,0 +1,14 @@
+package com.tnsif.packageandaccessmodifiers;
+
+import com.tnsif.packageTest.MainPackage;
+
+public class PackageTest {
+
+	public static void main(String[] args) {
+		
+		MainPackage mp = new MainPackage();
+		mp.display();
+
+	}
+
+}
