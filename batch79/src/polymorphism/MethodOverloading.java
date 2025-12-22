384d851901b1eee2a09d65ed7d33335c97b01d44
Gow#1234 <gowsica25@gmail.com>
2025-12-22 21:41:02 +0530

    init commit

---
+++ batch79/src/polymorphism/MethodOverloading.java
@@ -0,0 +1,26 @@
+package polymorphism;
+
+public class MethodOverloading {
+	
+	
+	//same method with different paratemeters
+	int addition(int a, int b)
+	{
+		return a + b;
+	}
+	
+	float addition(float a, float b)
+	{
+		return a + b;
+	}
+
+	public static void main(String[] args) {
+		
+		MethodOverloading mod = new MethodOverloading();
+		
+		System.out.println("Addtion is: "+mod.addition(5, 10));
+		System.out.println("Addtion is: "+mod.addition(5.5f, 7.7f));
+		
+		
+	}
+}
