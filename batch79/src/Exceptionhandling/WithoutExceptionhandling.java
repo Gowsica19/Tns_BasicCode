28acda62faf63f6db0dbb77e1abb1f148fab94b5
Gow#1234 <gowsica25@gmail.com>
2025-12-30 20:02:30 +0530

    init commit

---
+++ batch79/src/Exceptionhandling/WithoutExceptionhandling.java
@@ -0,0 +1,11 @@
+//Program without exception handling
+package Exceptionhandling;
+public class WithoutExceptionhandling {
+
+	public static void main(String[] args) {
+		System.out.println("the program continues....");
+		int data=100/0;
+		System.out.println("the program continues....");
+	}
+	
+}
